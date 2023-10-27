a23d83a3809e0038534a7d084256e883a9a49586
josealonso <jose.alonso.programmer@gmail.com>
2023-10-27 17:06:20 +0200

    Explained evaluation strategies in Scala.

---
+++ exercises/src/main/scala/lectures/Evaluation.scala
@@ -0,0 +1,23 @@
+package lectures
+
+object Evaluation {
+
+  // Expressions evaluation
+
+  val evaluatedWhenDefined = println("Only once")
+
+  lazy val evaluatedWhenFirstAccessed = println("Only once, but in a lazy way.")
+
+  // A method is evaluated every time is called
+  def myMethod = ???
+
+  // Parameters evaluation
+
+  // Normal (by-value) --> immediate evaluation
+  def otherMethod(a: Int): Unit = ???
+
+  // By-name --> evaluated every time they are  accessed
+  def otherMethod2(f: () => Int): Unit = ???
+  def otherMethod3(f : => Int): Unit = ???
+
+}
